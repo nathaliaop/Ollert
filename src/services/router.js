be28e308f2302c77a8d60bbe718969064a172aa1
Victor Manuel <victorvpn13@gmail.com>
2021-11-06 22:01:11 -0300

    feat: Cria rotas

--- src/services/router.js
+++ src/services/router.js
@@ -3,13 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 
 const Router = () => (
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Dashboard />}>
-
-            </Route>
-        </Routes>
-    </BrowserRouter>
-);
-
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Dashboard />}>
+      
+      </Route>
+    </Routes>
+  </BrowserRouter>
+  );
+  
 export default Router;
--- src/services/themes.js
+++ src/services/themes.js
@@ -1,28 +1,28 @@
 const Themes = {
-    'light': {
-
-    },
-    'dark': {
-        primary: '#BB86FC',
-        surface: '#2C2F33',
-        background: '#23272A',
-        white: '#FFFFFF',
-
-        grey50: '#FAFAFA',
-        grey100: '#F5F5F5',
-        grey200: '#EEEEEE',
-        grey300: '#E0E0E0',
-        grey400: '#BDBDBD',
-        grey500: '#9E9E9E',
-        grey600: '#757575',
-        grey700: '#616161',
-        grey800: '#424242',
-        grey900: '#212121',
-    },
-    'warm': {
-        grey50: '#FAF9F7',
-        grey500: '#857F72',
-    }
+  'light': {
+    
+  },
+  'dark': {
+    primary: '#BB86FC',
+    surface: '#2C2F33',
+    background: '#23272A',
+    white: '#FFFFFF',
+    
+    grey50: '#FAFAFA',
+    grey100: '#F5F5F5',
+    grey200: '#EEEEEE',
+    grey300: '#E0E0E0',
+    grey400: '#BDBDBD',
+    grey500: '#9E9E9E',
+    grey600: '#757575',
+    grey700: '#616161',
+    grey800: '#424242',
+    grey900: '#212121',
+  },
+  'warm': {
+    grey50: '#FAF9F7',
+    grey500: '#857F72',
+  }
 };
 
 export default Themes;
